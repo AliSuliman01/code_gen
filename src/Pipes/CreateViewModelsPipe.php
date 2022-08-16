184438bbdcbffbd543f0d789ea52981da8088c27
AliSliman <ali.sliman@ramaaz.com>
2022-08-16 08:59:54 +0300

    - add suffix to viewmodel name.

--- src/Pipes/CreateViewModelsPipe.php
+++ src/Pipes/CreateViewModelsPipe.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 
 class CreateViewModelsPipe
 {
-    private function handleSingleStub($stub, $json_file, $prefix)
+    private function handleSingleStub($stub, $json_file, $prefix, $suffix)
     {
 
         foreach ($json_file['tables'] as $table) {
@@ -17,7 +17,7 @@ class CreateViewModelsPipe
             $workingString = str_replace('{{ base_path }}', $table['base_path'], $workingString);
             $finalString = str_replace('{{ instance_name }}', Str::singular($table['table_name']), $workingString);
 
-            $file_name = $prefix . $table['entity_name'] . 'VM.php';
+            $file_name = $prefix . $table['entity_name'] . $suffix .'VM.php';
             $file_path = app_path('Http/ViewModels/' . str_replace('\\', '/', $table['base_path']) . '/' . $file_name);
             if (!is_dir(dirname($file_path)))
                 mkdir(dirname($file_path), 0777, true);
@@ -38,8 +38,8 @@ class CreateViewModelsPipe
         $getAllViewModelStub = file_get_contents(__DIR__ . '/../../stubs/vm/vm.get_all.stub');
         $getViewModelStub = file_get_contents(__DIR__ . '/../../stubs/vm/vm.get.stub');
 
-        $this->handleSingleStub($getAllViewModelStub, $json_file, 'GetAll');
-        $this->handleSingleStub($getViewModelStub, $json_file, 'Get');
+        $this->handleSingleStub($getAllViewModelStub, $json_file, 'GetAll','s');
+        $this->handleSingleStub($getViewModelStub, $json_file, 'Get','');
 
         return $next($json_file);
     }
