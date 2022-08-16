f8235c6a37eb94611b3491658b4b22169940c0ee
AliSliman <ali.sliman@ramaaz.com>
2022-08-16 04:03:52 +0300

    - register PublishCodeGenerateTemplate command.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,6 +5,7 @@ namespace Alisuliman\CodeGen;
 
 
 use Alisuliman\CodeGen\Commands\CodeGenerate;
+use Alisuliman\CodeGen\Commands\PublishCodeGenerateTemplate;
 use Illuminate\Contracts\Http\Kernel;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
@@ -13,7 +14,8 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot(Kernel $kernel)
     {
         $this->commands([
-            CodeGenerate::class
+            CodeGenerate::class,
+            PublishCodeGenerateTemplate::class
         ]);
 
         $this->mergeConfigFrom(__DIR__ . '/config/code_gen.php', 'code_gen');
