b90a94bfe60a643295b6e17dba3b4693a879f0bd
AliSliman <ali.sliman@ramaaz.com>
2022-08-28 04:08:08 +0300

    - append to postman collection instead of override it.

--- src/Pipes/GeneratePostmanCollectionPipe.php
+++ src/Pipes/GeneratePostmanCollectionPipe.php
@@ -19,20 +19,25 @@ class GeneratePostmanCollectionPipe
     public function handle($json_file, \Closure $next)
     {
 
-        $collectionStub = file_get_contents(__DIR__ . '/../../stubs/postman/collection.stub');
-        $collectionItemStub = file_get_contents(__DIR__ . '/../../stubs/postman/collection_item.stub');
+        $collectionArray = json_decode(file_get_contents(__DIR__ . '/../../stubs/postman/collection.json'), true);
+        $collectionItemStub = file_get_contents(__DIR__ . '/../../stubs/postman/collection_item.json');
+        $fileContent = json_decode(file_get_contents(base_path('postman_collection.json')), true);
 
-        $collectionItems = '';
+        $stringCollectionItems = '';
+
         foreach ($json_file['tables'] as $table) {
-            $collectionItem = str_replace('{{ table_name }}', $table['table_name'], $collectionItemStub);
-            $postman_json_raw = $this->get_json_raw($table['columns']);
-            $collectionItem = str_replace('{{ postman_json_raw }}', $postman_json_raw, $collectionItem);
-            $collectionItems .= (strlen($collectionItems) == 0 ? '':',').$collectionItem;
+            $stringCollectionItem = str_replace('{{ table_name }}', $table['table_name'], $collectionItemStub);
+            $stringCollectionItem = str_replace('{{ postman_json_raw }}', $this->get_json_raw($table['columns']), $stringCollectionItem);
+            $stringCollectionItems .= (strlen($stringCollectionItems) == 0 ? '':',').$stringCollectionItem;
         }
-        $collection = str_replace('{{ collection_items }}', "[$collectionItems]", $collectionStub);
+        $arrayCollectionItems = json_decode($stringCollectionItems,true);
+        if (isset($fileContent['item'])){
+            $arrayCollectionItems = array_merge($fileContent['item'], $arrayCollectionItems);
+        }
 
+        $collectionArray['item'] = $arrayCollectionItems;
         $file = fopen(base_path('postman_collection.json'), 'w');
-        fwrite($file, $collection);
+        fwrite($file, json_encode($collectionArray,JSON_UNESCAPED_SLASHES));
         fclose($file);
 
         echo "\e[0;32m postman_collection.json Generated.\e[0m\n";
